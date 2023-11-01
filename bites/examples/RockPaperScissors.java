076c2052a5534eed4a7e18d4d7ccd00e242893bb
xengeo <xen.geo4@gmail.com>
2023-11-01 18:18:10 +0000

    [SLUG] Transferring changes from Slug as of 2023-10-24 17:33:57 +0100.

--- bites/examples/RockPaperScissors.java
+++ bites/examples/RockPaperScissors.java
@@ -11,22 +11,23 @@ public class RockPaperScissors {
     public static void main(String[] args) {
 
         rules.put("rock", new HashMap<String, String>());
-        rules.get("rock").put("paper", "player 1 loses");
+        rules.get("rock").put("paper", "player 2 wins");
         rules.get("rock").put("scissors", "player 1 wins");
         rules.get("rock").put("rock", "it's a draw");
 
         rules.put("paper", new HashMap<String, String>());
         rules.get("paper").put("paper", "it's a draw");
-        rules.get("paper").put("scissors", "player 1 loses");
+        rules.get("paper").put("scissors", "player 2 wins");
         rules.get("paper").put("rock", "player 1 wins");
 
         rules.put("scissors", new HashMap<String, String>());
         rules.get("scissors").put("paper", "player 1 wins");
         rules.get("scissors").put("scissors", "it's a draw");
-        rules.get("scissors").put("rock", "player 1 loses");
+        rules.get("scissors").put("rock", "player 2 wins");
 
         System.out.println(rules.get("rock").get("paper"));
         System.out.println(rules.get("rock").get("scissors"));
         System.out.println(rules.get("paper").get("scissors"));
+        System.out.println(rules.get("scissors").get("scissors"));
     }
 }
